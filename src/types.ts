4c45fc1fa50c58bc4e024d3f19094a1a984974e4
oyvindsabo <oyvind.sabo@gmail.com>
2020-10-24 15:27:00 +0200

    Add initial code and add support for ES2017 and DOM

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+/**
+ * Same as Route, but with a hash at the beginning
+ */
+export type Hash     = string;
+export type Params   = Record<string, boolean | number | string>; // TODO: Check that boolean is actually supported
+/**
+ * Same as Hash, but without the hash at the beginning
+ */
+export type Route    = string;
+export type Title    = string;
+export type Token    = string;
+export type Callback = ({ params, currentRoute }: {
+  params: Params;
+  currentRoute: Route
+}) => void;
